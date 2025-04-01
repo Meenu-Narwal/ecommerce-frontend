d752a1d831947e1b819fcd1a768e724480b8372a
Meenu <meenunarwal935@gmail.com>
2025-04-01 23:18:29 +0530

    Initial project setup

--- scripts/app.js
+++ scripts/app.js
@@ -1,1 +1,8 @@
-console.log("E-Commerce Website Loaded");
+document.addEventListener('DOMContentLoaded', () => {
+    const hamburgerMenu = document.querySelector('.hamburger-menu');
+    const navMenu = document.querySelector('nav ul');
+
+    hamburgerMenu.addEventListener('click', () => {
+        navMenu.classList.toggle('active'); // Toggle the active class
+    });
+});
